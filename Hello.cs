77a0987eaa848418da4e589a72c7e8cf6c90fcea
ckalvaro <105681689+ckalvaro@users.noreply.github.com>
2023-10-23 11:41:52 -0300

    Add files via upload

---
+++ Hello.cs
@@ -0,0 +1,159 @@
+//==================================================
+// 
+//  Copyright 2020 Siemens Digital Industries Software
+//
+//==================================================
+
+
+using Teamcenter.hello23;
+using System;
+using Teamcenter.ClientX;
+using Teamcenter.Soa.Client.Model;
+using User = Teamcenter.Soa.Client.Model.Strong.User;
+using System.Collections.Generic;
+using Microsoft.Win32;
+using OfficeOpenXml;
+
+namespace Teamcenter.Hello
+{
+    public class ItemLine
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public int nivel { get; set; }
+        public string aux { get; set; }
+
+        public ItemLine(string id, string name, int nivel, string aux)
+
+        {
+            this.id = id;
+            this.name = name;
+            this.nivel = nivel;
+            this.aux = aux;
+        }
+
+    }
+
+    public class Hello
+    {
+       
+
+        public static void Main(string[] args)
+        {
+
+            String serverHost = "http://Server-PIL:85/tc";
+            List<ItemLine> registros = new List<ItemLine>();
+
+
+            if (args.Length > 0)
+            {
+                if (args[0].Equals("-help") || args[0].Equals("-h"))
+                {
+                    System.Console.Out.WriteLine("usage: Hello [-host http://server:port/tc]");
+                    return;
+                }
+                if (args[0].Equals("-host") && args.Length > 1)
+                {
+                    // Get optional host information
+                    serverHost = args[1];
+                }
+            }
+
+
+
+            try
+            {
+
+                Session session = new Session(serverHost);
+                HomeFolder home = new HomeFolder();
+                Query query = new Query();
+                DataManagement dm = new DataManagement();
+
+
+
+
+
+                // Establish a session with the Teamcenter Server
+                User user = session.login();
+
+                // Using the User object returned from the login service request
+                // display the contents of the Home Folder
+
+                //home.listHomeFolder(user);
+
+                // Perform a simple query of the database
+                ModelObject[] lista_objetos = query.queryItems();
+                Console.ReadLine();
+                int y = lista_objetos.Length;
+                for (int z = 0; z < y; z++)
+                {
+
+                    Console.WriteLine("-------*  UID  *-------");
+                    string UniqueId = lista_objetos[z].Uid.ToString();
+                    Console.WriteLine(UniqueId);
+                    ShowBOM showBOM = new ShowBOM(UniqueId);
+                    showBOM.loadItem();
+                    showBOM.showBOMWindowStructure(registros); // se llena lista supuestamente
+                }
+                // Perform some basic data management functions
+                //dm.createReviseAndDelete();
+                using (var package = new ExcelPackage())
+                {
+                    var worksheet = package.Workbook.Worksheets.Add("Registros");
+
+                    // Encabezados
+                    worksheet.Cells["A1"].Value = "ITEM ID";
+                    worksheet.Cells["B1"].Value = "NOMBRE";
+                    worksheet.Cells["D1"].Value = "NIVEL";
+                    worksheet.Cells["C1"].Value = "DESCRIPCION";
+
+                    // Llenar el archivo Excel con los datos de la lista de objetos
+                    int row = 2;
+                    foreach (var objeto in registros)
+                    {
+                        worksheet.Cells["A" + row].Value = objeto.id;
+                        worksheet.Cells["B" + row].Value = objeto.name;
+                        worksheet.Cells["C" + row].Value = objeto.aux;
+                        worksheet.Cells["D" + row].Value = objeto.nivel;
+                        row++;
+                    }
+
+                    // Guardar el archivo Excel en disco
+                    var file = new System.IO.FileInfo("Registros.xlsx");
+                    package.SaveAs(file);
+                }
+
+                Console.WriteLine("El archivo Excel ha sido creado.");
+
+                /*
+                foreach (ItemLine reg in registros) 
+                {
+                    Console.WriteLine($"Id: {reg.id} - Name: {reg.name} - Nivel: {reg.nivel} - AUX: {reg.aux}");
+                    Console.WriteLine("-- Enter para seguir --");
+                    Console.ReadLine();
+
+                }
+                */
+                // Show BOM
+                //ShowBOM showBOM = new ShowBOM("g6AAAEBeZOa2mD");
+                //showBOM.loadItem();
+                //showBOM.showBOMWindowStructure();
+                // Terminate the session with the Teamcenter server
+
+                session.logout();
+
+                Console.WriteLine("-- Enter para cerrar --");
+                Console.ReadLine();
+
+
+            }
+            catch (SystemException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
+    
+    
---
+++ showbom.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Teamcenter.Services.Strong.Core;
+using Teamcenter.Soa.Client.Model.Strong;
+using Teamcenter.Soa.Common;
+using Teamcenter.Soa.Client.Model;
+using Teamcenter.Soa.Exceptions;
+using Teamcenter.Services.Strong.Structuremanagement._2008_06.Structure;
+using System.Linq;
+using Teamcenter.Hello;
+
+
+namespace Teamcenter.hello23
+{
+
+    class VectorDatos
+    {
+        public List<string> Vector = new List<string>();
+        public void printVector()
+        {
+            Console.WriteLine("Vector: ");
+            for (int k = 0; k < Vector.Count; k++)
+            {
+                Console.WriteLine(Vector[k]);
+            }
+        }
+    }
+
+    
+
+    class ShowBOM
+    {
+        private String ItemUID; /*** user supplied Item UID with BOM structure ***/
+
+        private Teamcenter.Services.Strong.Core.DataManagementService dmService;
+        private Teamcenter.Services.Strong.Cad.StructureManagementService structureService;
+
+        private Item item;
+
+        
+
+        public ShowBOM(string itemUID)
+        {   
+            this.ItemUID = itemUID;
+            dmService = Teamcenter.Services.Strong.Core.DataManagementService.getService(Teamcenter.ClientX.Session.getConnection());
+            structureService = Teamcenter.Services.Strong.Cad.StructureManagementService.getService(Teamcenter.ClientX.Session.getConnection());
+
+            setObjectPolicy();
+        }
+
+        public bool loadItem()
+        {
+            ServiceData sData = dmService.LoadObjects(new string[] { ItemUID });
+
+            if (sData.sizeOfPlainObjects() > 0)
+            {
+                ModelObject mObj = sData.GetPlainObject(0);
+
+                if (mObj is Item)
+                {
+                    item = mObj as Item;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        /** public void printVector()
+        {
+            Console.WriteLine("Vector: ");
+            for (int k = 0; k < vector.Count; k++)
+            {              
+                Console.WriteLine(vector[k]);
+            }
+        } **/
+        public bool showBOMWindowStructure(List<ItemLine> registros)
+        {
+            try
+            {
+                ModelObject[] itemRevs = item.Revision_list;
+                ModelObject[] bomViews = item.Bom_view_tags;
+
+                foreach (ModelObject bomView in bomViews)
+                {
+                    foreach (ModelObject itemRev in itemRevs)
+                    {
+                        Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.CreateBOMWindowsInfo bomWinInfo = new Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.CreateBOMWindowsInfo();
+
+                        bomWinInfo.Item = item;
+                        bomWinInfo.ItemRev = (ItemRevision)itemRev;
+                        bomWinInfo.BomView = (PSBOMView)bomView;
+
+                        Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.CreateBOMWindowsResponse bomResp = structureService.CreateBOMWindows(new Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.CreateBOMWindowsInfo[] { bomWinInfo });
+
+                        if (bomResp.Output.Length > 0)
+                        {
+                            BOMLine bomLine = bomResp.Output[0].BomLine;
+                            
+
+                            expandBOMLines(bomLine, registros);
+                            //expandBOMAllLines(bomLine);
+                            
+                        }
+                    }
+                }
+
+                return true;
+            }
+            catch (NotLoadedException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+            return false;
+        }
+
+        private void expandBOMAllLines(BOMLine bomLine)
+        {
+            Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSAllLevelsInfo info = new Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSAllLevelsInfo();
+            Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSAllLevelsPref pref = new Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSAllLevelsPref();
+
+            info.ParentBomLines = new BOMLine[] { bomLine };
+            info.ExcludeFilter = "None";
+
+            pref.ExpItemRev = false;
+            pref.Info = new Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.RelationAndTypesFilter[0];
+
+            Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSAllLevelsResponse resp = structureService.ExpandPSAllLevels(info, pref);
+
+            foreach (Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSAllLevelsOutput data in resp.Output)
+            {
+                Console.WriteLine("Parent: " + data.Parent.BomLine.Bl_line_name); // + " -N " + data.Parent.BomLine.Bl_level_starting_0);
+
+                if (data.Children.Length > 0)
+                {
+                    foreach (Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSData child in data.Children)
+
+                        Console.WriteLine("\tChild: " + child.BomLine.Bl_line_name); //+ " -N " + child.BomLine.Bl_level_starting_0);
+                }
+                else
+                    Console.WriteLine("\tChildren: none");
+            }
+        }
+        private void expandBOMLines(BOMLine bomLine, List<ItemLine> registros)
+        {
+
+            
+
+            Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSOneLevelInfo levelInfo = new Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSOneLevelInfo();
+            Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSOneLevelPref levelPref = new Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSOneLevelPref();
+
+            levelInfo.ParentBomLines = new BOMLine[] { bomLine };
+            levelInfo.ExcludeFilter = "None";
+
+            levelPref.ExpItemRev = false;
+            levelPref.Info = new Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.RelationAndTypesFilter[0];
+
+            Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSOneLevelResponse levelResp = structureService.ExpandPSOneLevel(levelInfo, levelPref);
+            
+            //int contador = 0;
+            try
+            {
+                ItemLine objeto = new ItemLine(bomLine.Bl_item_item_id, bomLine.Bl_line_name, bomLine.Bl_level_starting_0, bomLine.Bl_rev_object_desc);
+                registros.Add(objeto);
+                //Vector.Add("BOMLine: " + bomLine.Bl_line_name);
+                //Vector.Add(" -N: " + bomLine.Bl_level_starting_0.ToString());
+                
+                
+                //contador++;
+                //Console.WriteLine("BOMLine: " + bomLine.Bl_line_name);
+            }
+            catch (NotLoadedException e)
+            {
+                Console.WriteLine("--* ERROR " + e.StackTrace);
+            }
+
+            if (levelResp.Output.Length > 0)
+            {
+                foreach (Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSOneLevelOutput levelOut in levelResp.Output)
+                {
+                    foreach (Teamcenter.Services.Strong.Cad._2007_01.StructureManagement.ExpandPSData psData in levelOut.Children)
+                    {
+                        expandBOMLines(psData.BomLine, registros);
+                    }
+                }
+            }
+        }
+
+        protected void setObjectPolicy()
+        {
+            SessionService session = SessionService.getService(Teamcenter.ClientX.Session.getConnection());
+            ObjectPropertyPolicy policy = new ObjectPropertyPolicy();
+
+            policy.AddType(new PolicyType("Item", new string[] { "bom_view_tags", "revision_list" }));
+            //ACA HAY QUE AGREGAR LOS DATOS QUE QUEREMOS RECUPERAR DEL ITEM, PARA GUARDAR EN LA CLASE BOMLine
+            policy.AddType(new PolicyType("BOMLine", new string[] { "bl_line_name", "bl_level_starting_0", "bl_item_item_id", "bl_rev_object_desc" }));
+
+            session.SetObjectPropertyPolicy(policy);
+        }
+    }
+}
